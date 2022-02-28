73660557bc489494bcc9aeca2c7131f33091ec21
Gene Wong <gene.h.wong@gmail.com>
2022-02-28 11:01:52 -0800

    implemented all classes with pseudoclassical instantiation pattern with passing tests for all

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,14 @@
 var Bee = function() {
+  //We dont want to just invoke one specific instance of Grub(new Grub()), because if we cahnge anything
+  //in Grub, it will not carry over to Bee. It's just ONE very specific instance if you do new Grub().
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+//you dont want it set to Grub.prototype, because that's just a pointer, and changing
+//bee's prototypes will change the pointers prototypes as well (In this case Grub!!);
+Bee.prototype = Object.create(Grub.prototype);
+//The prototype constructor should just be the constructor Bee itself!
+Bee.prototype.constructor = Bee;
